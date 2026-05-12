835114e0eaef2a0dea566c71639f4f7607ce7a03
COMRADE040 <rafiulislam040@gmail.com>
2026-05-12 23:54:10 +0600

    index.ts are initialized

---
+++ dist/src/index.ts
@@ -0,0 +1,31 @@
+// src/index.ts
+
+import { ZodSchema } from "zod";
+import { studentSchema } from "./schemas/student.schema";
+import { fakeStudentData } from "./data/fakeData";
+
+
+// reusable validation function
+const validateData = <T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T => {
+  return schema.parse(data);
+};
+
+try {
+  const validatedStudent = validateData(
+    studentSchema,
+    fakeStudentData
+  );
+
+  console.log("Validated Data:");
+  console.log(validatedStudent);
+
+} catch (error) {
+  console.log("Validation Error:");
+
+  if (error instanceof Error) {
+    console.log(error.message);
+  }
+}
