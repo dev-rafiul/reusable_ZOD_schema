53547251af5127a40c6687f3f7faf3b33ed186e1
COMRADE040 <rafiulislam040@gmail.com>
2026-05-12 23:53:31 +0600

    fake data created

---
+++ dist/src/data/fakeData.ts
@@ -0,0 +1,8 @@
+export const fakeStudentData = {
+  id: 1,
+  name: "Rafiul",
+  email: "rafiul@gmail.com",
+  age: 22,
+  studentId: "ST-101",
+  course: "Web Development",
+};
--- dist/src/types/student.type.ts
+++ dist/src/types/student.type.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
-import { studentSchema } from "../schemas/student.schema";
+import { studentSchema } from "../schemas/stodent.schema";
+
 
 export type Student = z.infer<typeof studentSchema>;
